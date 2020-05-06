986a5394cb327a50fd3e7ce2c952484a60347691
Gligor <pecevgligor@gmail.com>
2020-05-06 17:46:30 +0800

    newest commit

---
+++ src/attraction-components/FeaturedAttractions.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import AttractionEntryBox from '../attraction-components/AttractionEntryBox'
+import summerpalace1 from '../images/summer-palace-1.jpeg';
+import templeheaven1 from '../images/temple-heaven-1.jpg';
+import tianmen1 from '../images/tianmen-square-1.jpg';
+
+export const FeaturedAttractions = () => {
+
+
+    return (
+        <>
+        </>
+        // <div className='featured-attractions-home'>
+        //     <AttractionEntryBox introText={'A series of beautiful pavilions comprise this Mongolian-Tibetan yellow-sect temple, which features an impressive fifty-four-foot high Buddha carved from one piece of Tibetan sandalwood.'} title={'Summer palace'} img={summerpalace1} />
+        //     <AttractionEntryBox title={'Temple of Heaven'} img={templeheaven1} introText={'A series of beautiful pavilions comprise this Mongolian-Tibetan yellow-sect temple, which features an impressive fifty-four-foot high Buddha carved from one piece of Tibetan sandalwood.'}/>
+        //     <AttractionEntryBox title={'Tianmen Square'} img={tianmen1} introText={'A series of beautiful pavilions comprise this Mongolian-Tibetan yellow-sect temple, which features an impressive fifty-four-foot high Buddha carved from one piece of Tibetan sandalwood.'}/>
+        // </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../navigation-components/Hero'
 import Information from '../information-components/Information';
+import {FeaturedAttractions} from '../attraction-components/FeaturedAttractions'
 
 export default function Home() {
   return (
     <>
       <Hero title={'Beijing info hub'} subtitle={'Check out our tours'} buttonText={'Click me'} pathTo={'/tours'} />
       <Information />
+      <FeaturedAttractions />
     </>
   );
 }
